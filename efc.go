bc2d7707a2d29fe71b9d90e2973127e1761f49ee
Kosei Moriyama <cou929@gmail.com>
2019-07-07 21:37:52 +0900

    implement checker

--- efc.go
+++ efc.go
@@ -2,6 +2,8 @@ package efc
 
 import (
 	"go/ast"
+	"go/constant"
+	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -17,21 +19,84 @@ var Analyzer = &analysis.Analyzer{
 	},
 }
 
-const Doc = "efc is ..."
+const Doc = `efc reports print format which does not use "%+v" for error type.
+Intend to be applied to projects which uses pkg/errors.
+https://godoc.org/github.com/pkg/errors#hdr-Formatted_printing_of_errors
+`
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+		(*ast.CallExpr)(nil),
 	}
 
 	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			_ = n
+		call := n.(*ast.CallExpr)
+
+		format, idx := formatString(pass, call)
+		if idx < 0 {
+			return
+		}
+
+		argIdx := idx
+		errType := types.Universe.Lookup("error").Type()
+
+		for i := 0; i < len(format); i++ {
+			if format[i] != '%' {
+				continue
+			}
+			if i+1 < len(format) && format[i+1] == '%' {
+				i++
+				continue
+			}
+
+			argIdx++
+			if argIdx >= len(call.Args) {
+				break
+			}
+
+			arg := call.Args[argIdx]
+			typ := pass.TypesInfo.Types[arg].Type
+
+			if types.Identical(typ, errType) {
+				if len(format) < i+3 {
+					pass.Reportf(call.Pos(), "should use %%+v format for error type")
+					break
+				}
+				if format[i:i+3] != "%+v" {
+					pass.Reportf(call.Pos(), "should use %%+v format for error type")
+					break
+				}
+			}
 		}
 	})
 
 	return nil, nil
+}
+
+// copy from go vet printf
+// https://github.com/golang/tools/blob/72ffa07ba3db8d09f5215feec0f89464f3028f8e/go/analysis/passes/printf/printf.go#L352
+func formatString(pass *analysis.Pass, call *ast.CallExpr) (format string, idx int) {
+	for idx := range call.Args {
+		if s, ok := stringConstantArg(pass, call, idx); ok {
+			return s, idx
+		}
+		if pass.TypesInfo.Types[call.Args[idx]].Type == types.Typ[types.String] {
+			return "", -1
+		}
+	}
+	return "", -1
+}
+
+func stringConstantArg(pass *analysis.Pass, call *ast.CallExpr, idx int) (string, bool) {
+	if idx >= len(call.Args) {
+		return "", false
+	}
+	arg := call.Args[idx]
+	lit := pass.TypesInfo.Types[arg].Value
+	if lit != nil && lit.Kind() == constant.String {
+		return constant.StringVal(lit), true
+	}
+	return "", false
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,5 +1,47 @@
 package a
 
+import "fmt"
+
 func main() {
-	// want "pattern"
+	var err error
+	o := MyObj{}
+
+	// Okay
+	fmt.Printf("%d %s", 100, "abc")
+	fmt.Println("abc", 1, 2, 3)
+	myPrint("aa int %d, error %+v", 10, err)
+	o.Log("unexpected error. id = %d, err = %+v", 999, err)
+	myPrint2(999, "unexpected error. id = %d, err = %+v", 1, err)
+
+	// Not okay
+	fmt.Printf("%v", err)                                           // want `should use %\+v format for error type`
+	myPrint("int %d, error %#v", 10, err)                           // want `should use %\+v format for error type`
+	o.Log("unexpected error occurred. id = %d, err = %s", 999, err) // want `should use %\+v format for error type`
+
+	// Handling %%
+	fmt.Printf("%% %+v", err)
+
+	// Invalid directive or args.
+	// These are not valid for Printf but
+	// this checker will address only matching between`%+v` and error arg.
+	fmt.Printf("%+v", err, 1)
+	fmt.Printf("%+v %d", err)
+	fmt.Printf("%", err)         // want `should use %\+v format for error type`
+	fmt.Printf("abc % def", err) // want `should use %\+v format for error type`
+	fmt.Printf("%d, %", 1, err)  // want `should use %\+v format for error type`
+	fmt.Printf("%, %d", err, 1)  // want `should use %\+v format for error type`
+}
+
+func myPrint(s string, i int, e error) {
+	fmt.Printf(s, i, e)
+}
+
+type MyObj struct{}
+
+func (o *MyObj) Log(format string, args ...interface{}) {
+	fmt.Printf(format, args)
+}
+
+func myPrint2(i int, format string, args ...interface{}) {
+	fmt.Printf(format, args)
 }
